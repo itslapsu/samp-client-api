d82d42f411b411f68172c3d16d1de5c651a0227a
itslapsu <slaptashkaa@gmail.com>
2025-02-06 18:59:04 +0200

    on banned hook

--- src/samp/netgame.rs
+++ src/samp/netgame.rs
@@ -212,6 +212,30 @@ impl<'a> NetGame<'a> {
                 });
         }
     }
+
+    pub fn on_banned<F: FnMut() + 'static>(callback: F) {
+        let address = match version() {
+            Version::V037 => 0xA500,   //hz
+            Version::V037R3 => 0x8A00,  
+            Version::V03DLR1 => 0xA600, //hz
+            _ => return,
+        };
+    
+        unsafe {
+            let ptr = super::handle().add(address);
+            let func: extern "thiscall" fn(*mut (), *mut ()) = std::mem::transmute(ptr);
+    
+            let _ = GenericDetour::new(func, cnetgame_on_banned)
+                .map(|hook| {
+                    let _ = hook.enable();
+    
+                    ON_BANNED_HOOK = Some(CNetGameOnBannedHook {
+                        hook,
+                        callback: Box::new(callback),
+                    });
+                });
+        }
+    }
 }
 
 struct CNetGameDestroyHook {
@@ -304,6 +328,22 @@ static mut WRONG_PASSWORD_HOOK: Option<CNetGameWrongPasswordHook> = None;
 extern "thiscall" fn cnetgame_wrong_password(this: *mut (), packet: *mut ()) {
     unsafe {
         if let Some(hook) = WRONG_PASSWORD_HOOK.as_mut() {
+            (hook.callback)(); 
+            hook.hook.call(this, packet);
+        }
+    }
+}
+
+struct CNetGameOnBannedHook {
+    hook: GenericDetour<extern "thiscall" fn(*mut (), *mut ())>,
+    callback: Box<dyn FnMut()>,
+}
+
+static mut ON_BANNED_HOOK: Option<CNetGameOnBannedHook> = None;
+
+extern "thiscall" fn cnetgame_on_banned(this: *mut (), packet: *mut ()) {
+    unsafe {
+        if let Some(hook) = ON_BANNED_HOOK.as_mut() {
             (hook.callback)(); 
             hook.hook.call(this, packet);
         }
